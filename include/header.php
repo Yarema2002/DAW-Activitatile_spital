93b97c2342bf9346737dd14886e92649ca8334e9
Yarema2002 <156441585+Yarema2002@users.noreply.github.com>
2024-01-13 11:35:52 +0200

    Add files via upload

---
+++ include/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title> 
+    <link rel ="stylesheet" type="text/css"
+     href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.7.1.slim.js" 
+     integrity="sha256-UgvvN8vBkgO0luPSUl2s8TIlOSYRoGFAX4jlCIm9Adc=" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <link rel ="stylesheet" type="text/css"
+     href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.4.2/css/fontawesome.min.css">
+
+</head>     
+<body>
+    <nav class="navbar navbar-expand-lg navbar-info bg-info">
+        <h5 class="text-white">Hospital Management System</h5>
+        <div class="mr-auto"></div>
+
+        <ul class="navbar-nav">
+            <li class="nav-item"><a href="#" class="nav-link text-white">Admin</a></li>
+            <li class="nav-item"><a href="#" class="nav-link text-white">Doctor</a></li>
+            <li class="nav-item"><a href="#" class="nav-link text-white">Pacient</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
