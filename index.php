d794c596e68aec2ee3ff27d42cfa62367135128f
Yarema2002 <156441585+Yarema2002@users.noreply.github.com>
2024-01-13 11:28:57 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
-<?php
-include("include/header.php");
-?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,6 +6,37 @@ include("include/header.php");
     <title>HMS home Page</title>
 </head>
 <body>
-    
+    <?php
+include("include/header.php");
+?>
+<div style="margin-top: 50px"></div>
+<div class="container">
+    <div class="col-md-12">
+        <div class="row">
+            <div class="col-md-3 mx-1 shadow">
+                <img src="img/info.jpeg">
+            </div>
+            <div class="col-md-4 mx-1 shadow">
+                <img src="img/patient.jpeg" style="width: 100%;">
+
+                <h5 class="text-center">Create account so that we can take good care of you</h5>
+
+                <a href="#">
+                    <button class="btn btn-success my-3" style="margin-left: 30%;">Create account!!</button>
+                </a>
+            </div>
+
+            <div class="col-md-4 mx-1 shadow">
+                <img src="img/doctor.jpg" style="width: 100%;">
+
+                <h5 class="text-center">We are employing for doctors</h5>
+
+                <a href="#">
+                    <button class="btn btn-success my-3" style="margin-left: 30%;">Apply Now!!</button>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
 </body>
 </html>
