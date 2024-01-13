149b57a03683868dc9e867fbcedebe257572fc5f
Yarema2002 <156441585+Yarema2002@users.noreply.github.com>
2024-01-13 11:35:22 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -14,7 +14,14 @@ include("include/header.php");
     <div class="col-md-12">
         <div class="row">
             <div class="col-md-3 mx-1 shadow">
-                <img src="img/info.jpeg">
+                <img src="img/info.jpeg" style="width: 100%;height: 190px">
+
+                <h5 class="text-center">Click on the button for more information</h5>
+
+                <a href="#">
+                    <button class="btn btn-success my-3" style="margin-left: 20%;">More Information</button>
+                </a>
+
             </div>
             <div class="col-md-4 mx-1 shadow">
                 <img src="img/patient.jpeg" style="width: 100%;">
